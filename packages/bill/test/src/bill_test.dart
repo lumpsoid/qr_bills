458306738df33d59a967bbd447d2471c8ba68bd8
lumpsoid <mmgeeki@gmail.com>
2024-04-19 23:38:48 +0200

    test: commented all (no time to be safe)

--- packages/bill/test/src/bill_test.dart
+++ packages/bill/test/src/bill_test.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:bill/bill.dart';
 import 'package:test/test.dart';
 
 void main() {
--- packages/bill_repository/test/src/bill_repository_test.dart
+++ packages/bill_repository/test/src/bill_repository_test.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:bill_repository/bill_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
