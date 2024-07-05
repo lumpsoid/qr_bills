6549b5670d5f16d51830357be7970009758fcb50
lumpsoid <mmgeeki@gmail.com>
2024-07-05 16:04:38 +0200

    feat: add dev flavour

---
+++ lib/main_dev.dart
@@ -0,0 +1,26 @@
+import 'package:bill_rest_api/bill_rest_api.dart';
+import 'package:bill_sqflite_api/bill_sqflite_api.dart';
+import 'package:flutter/widgets.dart';
+import 'package:settings_shared_api/settings_shared_api.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:qr_bills/bootstrap.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final settingsApi = SettingsSharedApi(
+      plugin: await SharedPreferences.getInstance(),
+      settings: const Settings(
+        isDarkTheme: true,
+        serverUrl: '192.168.1.20:8080',
+      ));
+  final billsLocalApi = BillSqfliteApi()..initDb();
+  const billsRestApi = BillRestApi();
+
+  bootstrap(
+    settingsApi: settingsApi,
+    billSqfliteApi: billsLocalApi,
+    billRestApi: billsRestApi,
+  );
+}
